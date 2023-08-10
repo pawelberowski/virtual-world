5a4543917aadeb6ef679d506eac036306772d52d
Taz <pawelberowski@gmail.com>
2023-08-10 13:10:07 +0200

    Added move in random direction

--- src/Animal.js
+++ src/Animal.js
@@ -7,7 +7,16 @@ export class Animal extends Organism {
     this.board = board;
   }
 
+  getRandomDirection() {
+    const directionsArray = ['n', 'ne', 'e', 'se', 's', 'sw', 'w', 'nw'];
+    return directionsArray[Math.floor(Math.random() * directionsArray.length)];
+  }
+  // check for walls in action and pick another
   action() {
-    this.board.moveOrganism('e', this.tile.xCoordinate, this.tile.yCoordinate);
+    this.board.moveOrganism(
+      this.getRandomDirection(),
+      this.tile.xCoordinate,
+      this.tile.yCoordinate,
+    );
   }
 }
