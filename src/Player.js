8e265288677231504a458d1aba414e752db6cf54
Taz <pawelberowski@gmail.com>
2023-08-18 01:07:13 +0200

    Added alternate way of moving for player

--- src/Player.js
+++ src/Player.js
@@ -11,31 +11,31 @@ export class Player extends Animal {
   getDirection() {
     return new Promise((resolve) => {
       document.addEventListener('keypress', (event) => {
-        if (event.code === 'Numpad1') {
+        if (event.code === 'Numpad1' || event.code === 'KeyZ') {
           resolve('sw');
         }
-        if (event.code === 'Numpad2') {
+        if (event.code === 'Numpad2' || event.code === 'KeyX') {
           resolve('s');
         }
-        if (event.code === 'Numpad3') {
+        if (event.code === 'Numpad3' || event.code === 'KeyC') {
           resolve('se');
         }
-        if (event.code === 'Numpad4') {
+        if (event.code === 'Numpad4' || event.code === 'KeyA') {
           resolve('w');
         }
-        if (event.code === 'Numpad5') {
+        if (event.code === 'Numpad5' || event.code === 'KeyS') {
           resolve('skipTurn');
         }
-        if (event.code === 'Numpad6') {
+        if (event.code === 'Numpad6' || event.code === 'KeyD') {
           resolve('e');
         }
-        if (event.code === 'Numpad7') {
+        if (event.code === 'Numpad7' || event.code === 'KeyQ') {
           resolve('nw');
         }
-        if (event.code === 'Numpad8') {
+        if (event.code === 'Numpad8' || event.code === 'KeyW') {
           resolve('n');
         }
-        if (event.code === 'Numpad9') {
+        if (event.code === 'Numpad9' || event.code === 'KeyE') {
           resolve('ne');
         }
       });
