97580a37a2ec0595597f192b79ae545040e959b3
Taz <pawelberowski@gmail.com>
2023-08-18 00:46:31 +0200

    Added player movement

--- src/Player.js
+++ src/Player.js
@@ -7,4 +7,55 @@ export class Player extends Animal {
     this.initiative = 4;
     this.cssClass = 'player';
   }
+
+  getDirection() {
+    return new Promise((resolve) => {
+      document.addEventListener('keypress', (event) => {
+        if (event.code === "Numpad1") {
+          resolve('sw');
+        }
+        if (event.code === "Numpad2") {
+          resolve('s');
+        }
+        if (event.code === "Numpad3") {
+          resolve('se');
+        }
+        if (event.code === "Numpad4") {
+          resolve('w');
+        }
+        if (event.code === "Numpad5") {
+          resolve('skipTurn');
+        }
+        if (event.code === "Numpad6") {
+          resolve('e');
+        }
+        if (event.code === "Numpad7") {
+          resolve('nw');
+        }
+        if (event.code === "Numpad8") {
+          resolve('n');
+        }
+        if (event.code === "Numpad9") {
+          resolve('ne');
+        }
+      })
+    })
+  }
+
+  action() {
+    return new Promise((resolve) => {
+      this.getDirection().then((direction) => {
+        if (direction === 'skipTurn') {
+          return resolve();
+        }
+        const newTile = this.board.probeForNewTile(
+          direction,
+          this.tile,
+        );
+        this.board.moveOrganism(this.tile, newTile);
+        return resolve();
+      })
+    })
+  }
+
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { SowThistle } from './SowThistle';
 import { Grass } from './Grass';
 import { PoisonBerry } from './PoisonBerry';
 import { Guarana } from './Guarana';
+import {Player} from "./Player";
 
 const board = new Board();
 board.generateBoard();
@@ -17,13 +18,13 @@ board.addOrganism(0, 19, Grass);
 board.addOrganism(19, 0, PoisonBerry);
 board.addOrganism(19, 19, Guarana);
 
-board.addOrganism(10, 10, Sheep);
+board.addOrganism(10, 10, Player);
 // board.addOrganism(0, 19, Fox);
-board.addOrganism(11, 10, Sheep);
-board.addOrganism(10, 11, Sheep);
-board.addOrganism(9, 11, Sheep);
-// board.addOrganism(0, 10, Fox);
-board.addOrganism(1, 10, Fox);
+// board.addOrganism(11, 10, Sheep);
+// board.addOrganism(10, 11, Sheep);
+// board.addOrganism(9, 11, Sheep);
+// // board.addOrganism(0, 10, Fox);
+// board.addOrganism(1, 10, Fox);
 // board.addOrganism(0, 8, Antelope);
 // board.addOrganism(0, 6, Turtle);
 // board.addOrganism(0, 4, Player);
